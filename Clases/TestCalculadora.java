3d0dfd84fcc42695d0d84b342578fd86bcc90f20
KernyMC <kalexandervargas@gmail.com>
2023-12-09 16:55:19 -0500

    Probar las operaciones en calculadora

--- Clases/TestCalculadora.java
+++ Clases/TestCalculadora.java
@@ -3,11 +3,21 @@ public class TestCalculadora {
     public static void main(String[] args) {
         Calculadora calcu;
         int suma, resta;
+        double division, multiplicacion, promedio;
         calcu = new Calculadora();
         suma = calcu.sumar(1, 5);
         resta = calcu.restar(3, 6);
+        division = calcu.dividir(10, 2);
+        multiplicacion = calcu.multiplicar(5, 10);
+        promedio = calcu.promediar(10, 8, 9);
         
         System.out.println("La suma es: " + suma);
         System.out.println("La resta es: " + resta);
+        System.out.println("La division es: " + division);
+        System.out.println("La multiplicacion es: " + multiplicacion);
+        System.out.println("El promedio es: " + promedio);
+        calcu.mostrarResultado();
+
+
     }
 }
