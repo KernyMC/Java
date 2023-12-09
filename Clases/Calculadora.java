5f546f8c860658b11d4291decdbbee374cde8439
KernyMC <kalexandervargas@gmail.com>
2023-12-09 16:50:39 -0500

    operacion multiplicar

--- Clases/Calculadora.java
+++ Clases/Calculadora.java
@@ -12,6 +12,13 @@ public class Calculadora {
         return resultado;
     }
 
+    public double multiplicar(double valor1, double valor2) {
+        double resultado;
+        resultado = valor1 * valor2;
+        return resultado;
+    }
+
+
 
     
 }
