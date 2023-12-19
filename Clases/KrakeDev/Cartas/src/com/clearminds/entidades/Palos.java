dd0f9544b870cf8c0874d54e79877fb066b78289
KernyMC <kalexandervargas@gmail.com>
2023-12-19 18:21:18 -0500

    Ejercicio 1 - Creacion de Palos, Numeros y Cartas

---
+++ Clases/KrakeDev/Cartas/src/com/clearminds/entidades/Palos.java
@@ -0,0 +1,26 @@
+package com.clearminds.entidades;
+
+public class Palos {
+    private String corazonRojo = "CR";
+    private String corazonNegro = "CN";
+    private String diamante = "DI";
+    private String trebol= "TR";
+
+    public Palos() {}
+
+    public String getCorazonRojo() {
+        return corazonRojo;
+    }
+
+    public String getCorazonNegro() {
+        return corazonNegro;
+    }
+
+    public String getDiamante() {
+        return diamante;
+    }
+
+    public String getTrebol() {
+        return trebol;
+    }
+}
